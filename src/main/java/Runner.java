44813f377ce0e24c0cbfe88709073a0d8dd2ac62
RNWalker <rebecca.noelia.walker@gmail.com>
2023-07-10 14:30:59 +0100

    MVP task

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -15,22 +15,33 @@ public class Runner {
         scottishIslands.add("Tresco");
 
 //        1. Add "Coll" to the end of the list
+        scottishIslands.add("Coll");
 //        2. Add "Tiree" to the start of the list
+        scottishIslands.add(0, "Tiree");
 //        3. Add "Islay" after "Jura" and before "Mull"
+        scottishIslands.add(2, "Islay");
 //        4. Print out the index position of "Skye"
+        scottishIslands.indexOf("Skye");
 //        5. Remove "Tresco" from the list by name
+        scottishIslands.remove("Tresco");
 //        6. Remove "Arran" from the list by index
+        scottishIslands.remove(5);
 //        7. Print the number of islands in your arraylist
+        System.out.println(scottishIslands.size());
 //        8. Sort the list alphabetically
+        Collections.sort(scottishIslands);
 //        9. Print out all the islands using a for loop
+        for (int i = 0; i < scottishIslands.size() ; i++) {
+            System.out.println(scottishIslands.get(i));
 
-        System.out.println(scottishIslands);
+        };
+        }
 
 //        NUMBERS
-        List<Integer> numbers = new ArrayList<>();
-        Collections.addAll(numbers, 1, 1, 4, 2, 7, 1, 6, 15, 13, 99, 7);
-
-        System.out.println("numbers: " + numbers);
+//        List<Integer> numbers = new ArrayList<>();
+//        Collections.addAll(numbers, 1, 1, 4, 2, 7, 1, 6, 15, 13, 99, 7);
+//
+//        System.out.println("numbers: " + numbers);
 
 //        1. Print out a list of the even integers
 //        2. Print the difference between the largest and smallest value
@@ -44,4 +55,3 @@ public class Runner {
 
     }
 
-}
